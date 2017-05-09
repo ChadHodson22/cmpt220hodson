cf378568f3900af6b345fbbe13389ba546776d10
ChadHodson22 <chad.hodson1@marist.edu>
2017-05-09 13:29:45 -0400

    Add files via upload

--- labs/Lab9/src/ETest.java
+++ labs/Lab9/src/ETest.java
@@ -1,4 +1,6 @@
 
-public class ETest {
-
+public class ETest extends IllegalTriangleException{
+	public static void main(String[] args) throws IllegalTriangleException {
+		Triangle triangle= new Triangle(2.0, 3.0, 10.0);
+		}
 }
--- labs/Lab9/src/IllegalTriangleException.java
+++ labs/Lab9/src/IllegalTriangleException.java
@@ -1,4 +1,6 @@
 
-public class IllegalTriangleException {
-
+public class IllegalTriangleException extends Exception {
+	public void IllegalTraiangleException() {
+		System.out.println("sum of two sides is greater than "+"the other side");
+		}
 }
