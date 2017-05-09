507b5ec0add586089d83a7a48b33808309b5b80a
ChadHodson22 <chad.hodson1@marist.edu>
2017-05-08 23:40:48 -0400

    Add files via upload

---
+++ labs/Lab11/src/Problem1811.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Problem1811 {
+	static int sum;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.print("Enter the number: ");
+			Scanner input = new Scanner(System.in);
+			String str = input.next();
+			int num = Integer.parseInt(str);
+			System.out.print("The sum is: "+sumDigits(num));
+		}
+		catch(Exception e) {
+			System.out.println("Exception has occurred. Program will exit. ");
+			System.exit(0);
+		}
+	}
+	public static int sumDigits(long n) {
+		if(n > 0) {
+			sum = sum+(int)n%10;
+			sumDigits(n/10);
+		}
+		return sum;
+	}
+}
